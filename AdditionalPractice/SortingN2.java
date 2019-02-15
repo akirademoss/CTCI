3e26598ae8ce62ff7d1eadeea9ab5a20e3f8203b
akira <akirademoss@gmail.com>
2019-02-14 19:56:20 -0600

    added some sorting algorithm code

---
+++ AdditionalPractice/SortingN2.java
@@ -0,0 +1,40 @@
+package AdditionalPractice;
+
+public class SortingN2 {
+    
+
+
+
+    public static void main(String[] args){
+    
+        int[] array = {6,3,9,8,5,1};
+    
+        
+
+    
+   
+        
+        int smallest = getSmallestN2(array);
+    
+        System.out.println("The smallest integer is: " + smallest);
+    }
+
+
+    public static int getSmallestN2(int[] arr){
+        for(int i = 0; i<arr.length; i++){
+            int temp;
+            for (int j = 0; j<arr.length-1; j++){
+                if(arr[i] < arr[j]){
+                    temp = arr[j]; 
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+        return arr[0];
+    }
+    
+    public static int getSmallest(int[] arr){
+        return 0;
+    }
+}
